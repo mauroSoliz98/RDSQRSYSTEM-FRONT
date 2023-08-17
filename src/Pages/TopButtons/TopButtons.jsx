1a5ca4d9c05f467023a0e67d833944d2146e906e
mauroSoliz98 <maurososoliz@gamail.com>
2023-08-16 21:38:07 -0400

    Se puso el date Time Picker y el boton para descargar reportes

--- src/Pages/TopButtons/TopButtons.jsx
+++ src/Pages/TopButtons/TopButtons.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-import { DownloadOutlined, PlusOutlined } from '@ant-design/icons';
-import { Input, Button } from 'antd';
+import { DownloadOutlined, PlusOutlined} from '@ant-design/icons';
+import { Input, Button, Row, Col,DatePicker} from 'antd';
+import { ButtonContainer } from '../../Styles';
+import dayjs from 'dayjs'
 const { Search } = Input;
 function TopButtons({onClick}) {
+  const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
   return (
-    <div style={{ flexDirection: window.innerWidth <= 600 ? 'column' : 'row', marginBottom: 8, display: 'flex' }}>
-    <Search
-      placeholder="Buscar"
-      style={{
-        flex: '1',
-        marginBottom: window.innerWidth <= 600 ? 8 : 0,
-        marginRight: window.innerWidth <= 600 ? 0 : 8,
-        transition: 'margin 0.3s ease-in-out',
-      }}
-    />
-    <div style={{ alignItems: window.innerWidth <= 600 ? 'flex-start' : 'center', justifyContent: 'flex-end', display: 'flex', flex: '1' }}>
-      <Button type="primary" icon={<PlusOutlined />} onClick={onClick} style={{ marginLeft: 4, width: window.innerWidth <= 600 ? '100%' : 'auto' }}>
-        Nuevo registro
-      </Button>
-    </div>
+    <div>
+    <Row justify="space-between" style={{ marginBottom: 16 }}>
+          <Col>
+          <Search placeholder="Buscar" style={{ width: 200 }}/>
+          </Col>
+          <Col>
+            <ButtonContainer direction="horizontal">
+                <Button type="default" icon={<DownloadOutlined />}>Reporte</Button>
+                <Button type="primary" icon={<PlusOutlined />} onClick={onClick}>Nuevo registro</Button>
+                <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} />
+            </ButtonContainer>
+          </Col>
+        </Row>
   </div>
   )
 }
