6339c8bec32fea6d1f341ef5a0a0828a3ff6a313
mauroSoliz98 <maurososoliz@gamail.com>
2023-08-16 21:18:28 -0400

    Ya se logra divisar el menu

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import logo from '../../assets/Logo_company.jpeg'
-import { Avatar,Layout, Typography} from 'antd'
-import {UserOutlined} from '@ant-design/icons'
+import { Avatar,Layout, Typography, Popover, Button} from 'antd'
+import {UserOutlined,LogoutOutlined} from '@ant-design/icons'
 const { Header } = Layout;
 
 export function AppHeader() {
+  const content = (
+    <div>
+      <Button  
+        style={{ 
+                  border:'none', 
+                  backgroundColor:'transparent',
+                  height: 'fit-content', 
+                  width: 'fit-content'
+                }} 
+        icon={<LogoutOutlined />}
+      >
+        Cerrra sesion
+      </Button>
+    </div>
+  )
   return (
-      <Header className='AppHeader'>
-        <div className='logo'>
-            <img src={logo} alt="logotipo" className='icon' />
-        </div>
-        <div className='UserContainer'>
-          <Avatar shape="square" size={60} icon={<UserOutlined />} />
-          <Typography.Text style={{color:'white'}}>Admin</Typography.Text>
-        </div>
-    </Header>
+    <>
+    <Header className='AppHeader'>
+      <div className='logo'>
+          <img src={logo} alt="logotipo" className='icon' />
+      </div>
+      <Popover style={{height: 'fit-content', width: 'fit-content'}} placement="bottomRight" arrow = {false} content={content} trigger={'click'}>
+        <Button className='UserContainer'>
+          <Avatar shape="square" size={30} icon={<UserOutlined />} />
+          <Typography.Text style={{ color: 'white', marginLeft: '10px' }}>Admin</Typography.Text>
+        </Button>
+      </Popover>
+  </Header>
+</>
   )
 }
