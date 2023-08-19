3e8e99a52a6e9405fa32cfb45978b36cfa8ee1ab
melina-RG <m.gomezferrufino@gmail.com>
2023-08-19 09:26:21 -0400

    implementacion goBack

--- src/components/TopNav/index.jsx
+++ src/components/TopNav/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import { Breadcrumb, Typography } from 'antd'
+import { Breadcrumb, Typography, Button } from 'antd'
+import { ArrowLeftOutlined } from '@ant-design/icons';
 import { useMediaQuery } from 'react-responsive';
+import { useLocation } from 'react-router-dom';
 
 
 export function TopNav({selectedSection}) {
   const isDesktop = useMediaQuery({ minWidth: 768 }); // Define el ancho mínimo para la vista de escritorio
+  const location = useLocation();
+
+  const goBack = () => {
+    window.history.back(); // Retrocede en la historia
+  };
 
   return (
     <div style={{backgroundColor:'white', borderBlockEnd:50}}>
+      <div style={{display: 'flex', alignItems: 'baseline'}}>
+      {location.pathname === '/home/elementos_listar' && (
+            <Button type="link" onClick={goBack} icon={<ArrowLeftOutlined />}>
+            </Button>
+          )}
       <Breadcrumb style={{margin: '16px 16px',}}>
         <Breadcrumb.Item>Inicio</Breadcrumb.Item>
         <Breadcrumb.Item>{selectedSection}</Breadcrumb.Item>
       </Breadcrumb>
+      </div>
+      
       {isDesktop && <Typography.Title level={3} style={{ margin: '16px' }}>{selectedSection}</Typography.Title>}
+     
     </div>
   )
 }
